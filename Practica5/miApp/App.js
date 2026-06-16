86eb1a320ba1a2045df8f715cb4bdb27196bc956
JonathanReyesC <139170691+JonathanReyesC@users.noreply.github.com>
2026-06-15 20:05:22 -0600

    Practica No.8: StyleSheet

--- Practica5/miApp/App.js
+++ Practica5/miApp/App.js
@@ -1,29 +1,53 @@
-//Zona 1: Importaciones de archivos y Componentes
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Saludo } from './components/Saludo';
-import { Saludo2 } from './components/Saludo2';
+import { StyleSheet, View } from 'react-native';
 import { Perfil } from './components/Perfil';
 
-//Zona 2: Main - componentes 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <Perfil nombre="Jonathan Carbajal Reyes" carrera="Ingeniería en Sistemas Computacionales" materia="Programación Móvil" cuatrimestre="9" />
-      <Perfil nombre="Santiago Amaya" carrera="Ingeniería en Sistemas Computacionales" materia="Programación Movil" cuatrimestre="9" />
+      <Perfil 
+        style={styles.tarjetaVerde} 
+        nombre="Jonathan Carbajal Reyes" 
+        carrera="Ingeniería en Sistemas Computacionales" 
+        materia="Programación Móvil" 
+        cuatrimestre="9" 
+      />
+
+      
+      <Perfil 
+        style={styles.tarjetaRoja} 
+        nombre="Santiago Amaya" 
+        carrera="Ingeniería en Sistemas Computacionales" 
+        materia="Programación Movil" 
+        cuatrimestre="9" 
+      />
+
+           <Perfil 
+        style={styles.tarjetaVerde} 
+        nombre="Jonathan Carbajal Reyes" 
+        carrera="Ingeniería en Sistemas Computacionales" 
+        materia="Programación Móvil" 
+        cuatrimestre="9" 
+      />
 
       <StatusBar style="auto" />
     </View>
   );
 }
 
-//Zona 3: Estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row' , 
+  },
+  tarjetaVerde: {
+    backgroundColor: 'green' 
+  },
+  tarjetaRoja: {
+    backgroundColor: 'red' 
   },
 });
--- Practica5/miApp/components/Perfil.js
+++ Practica5/miApp/components/Perfil.js
@@ -1,35 +1,51 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 
 
-/* Perfil usando desestructuracion */
-import {View, Text, Button} from 'react-native';
-import React, {useState} from 'react';
-
- export const Perfil = ({nombre, carrera, materia, cuatrimestre}) => {
-    const[mostrar, setMostrar] = useState(false);
+export const Perfil = ({nombre, carrera, materia, cuatrimestre, style}) => {
+    const [mostrar, setMostrar] = useState(false);
+    
     return(
-        <View>  
-            <Text>{nombre}</Text>
+        
+        <View style={[estilos.tarjeta, style]}>  
+            <Text style={estilos.nombre}>{nombre}</Text>
 
             {mostrar &&
             <>
-            <Text>{carrera}</Text>
-            <Text>{materia}</Text>
-            <Text>{cuatrimestre}</Text>
+                <Text style={estilos.carrera}>{carrera}</Text>
+                <Text style={estilos.otroTexto}>{materia}</Text>
+                <Text style={estilos.otroTexto}>{cuatrimestre}</Text>
             </>
             }
 
             <Button title="Ver Perfil" onPress={() => setMostrar(!mostrar)}/>
         </View>
     );
- }
+}
 
-/* export const Perfil= (props) => {
-    return(
-        <View>  
-            <Text>{props.nombre}</Text>
-            <Text>{props.carrera}</Text>
-            <Text>{props.materia}</Text>
-            <Text>{props.cuatrimestre}</Text>
-        </View>
-    );
-} */
+const estilos = StyleSheet.create({
+    nombre: {
+        fontSize: 24,
+        fontWeight: '600',
+        textTransform: 'uppercase',
+        color: 'white' 
+    },
+    carrera: {
+        fontSize: 18,
+        color: 'white',
+        fontFamily: 'Roboto',
+    },
+    otroTexto: {
+        fontSize: 12,
+        color: 'white',
+        fontFamily: 'Courier',
+        fontStyle: 'italic',
+    },
+    tarjeta: {
+        borderWidth: 2,
+        borderColor: 'black',
+        padding: 25,
+        margin: 20,
+        backgroundColor: 'blue' 
+    },
+});
